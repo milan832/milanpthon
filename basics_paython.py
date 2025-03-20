1e3efa47de1fd0ab9d3e9c463878edae0b11b290
milan832 <milanvarunivzen@gmail.com>
2025-03-20 11:31:24 +0530

    Update basics_paython.py

--- basics_paython.py
+++ basics_paython.py
@@ -5,3 +5,25 @@ height = 5.7
 print( name)
 print( age)
 print( height)
+
+#strings
+company  = 'nivzen technologies'
+print(len(company ))
+print(company [0:18])
+print(company [0:18:2])
+print(company [::-1])#reverse string
+print(company [:5])
+
+#escape character:\,\',\",\\
+course= "devops engineer"
+print(len(course))#length of string
+print(course.upper())#convert string in uppercase also Lower() for lowercase
+print(course.replace("e","n"))
+
+#numbers
+x = 20
+print(x+2)
+print(x*2)
+print(x/2)
+print(x-2)
+print(x**2)
